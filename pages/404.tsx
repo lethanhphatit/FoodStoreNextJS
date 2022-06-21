6e295abb6be1f3d1eec8869a6a0f8a5184e61662
Phat le <phat.le.set14@eiu.edu.vn>
2022-06-21 16:01:18 +0700

    Update - 01

---
+++ pages/404.tsx
@@ -0,0 +1,11 @@
+import { Component } from "react"
+
+export default class extends Component {
+  render () {
+    return (
+      <div style={{padding: "15px"}}>
+        <span>404 - Page Not Found.</span>
+      </div>
+    )
+  }
+}
