ba3f87d53b71d0eb8fafee9f82e6738f5514325a
Phat le <phat.le.set14@eiu.edu.vn>
2022-06-21 13:17:13 +0700

    update commit

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import * as React from "react";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import Script from "next/script";
+import Script from 'next/script'
 
 const theme = createTheme();
 export default function Layout({ children }) {
@@ -18,29 +18,23 @@ export default function Layout({ children }) {
           property="og:image"
           content="https://food-store-next-js.vercel.app/favicon.ico"
         />
-        <meta
-          property="og:url"
-          content="https://food-store-next-js.vercel.app/"
-        />
+        <meta property="og:url" content="https://food-store-next-js.vercel.app/" />
+        <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-XXXXX-Y', 'auto');
+          ga('send', 'pageview');
+        `}
+      </Script>
       </Head>
       <main>
         <ThemeProvider theme={theme}>{children}</ThemeProvider>
       </main>
 
-      <Script
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-      
-      ga('create', 'UA-XXXX-Y', 'auto');
-      ga('send', 'pageview');
-  `,
-        }}
-      />
     </>
   );
 }
