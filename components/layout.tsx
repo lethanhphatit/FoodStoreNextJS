ad8dc6969fe6ee73e73d83bf0dbe90e04e711798
Phat le <phat.le.set14@eiu.edu.vn>
2022-06-21 13:19:38 +0700

    Update commit

--- components/layout.tsx
+++ components/layout.tsx
@@ -19,17 +19,7 @@ export default function Layout({ children }) {
           content="https://food-store-next-js.vercel.app/favicon.ico"
         />
         <meta property="og:url" content="https://food-store-next-js.vercel.app/" />
-        <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-          ga('create', 'UA-XXXXX-Y', 'auto');
-          ga('send', 'pageview');
-        `}
-      </Script>
+       
       </Head>
       <main>
         <ThemeProvider theme={theme}>{children}</ThemeProvider>
