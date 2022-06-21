01cbcf4f100f10e062be3144ff28199d205f2e4c
Phat le <phat.le.set14@eiu.edu.vn>
2022-06-21 13:21:04 +0700

    Update Commit

--- pages/menu/[id].tsx
+++ pages/menu/[id].tsx
@@ -9,6 +9,7 @@ import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import AppBar from "@mui/material/AppBar";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Script from "next/script";
 
 // id: "Burger_King_Combo_1",
 //     name: "Burger King Combo 1",
@@ -45,8 +46,19 @@ export default function MediaCard() {
               justifyContent: "center",
             }}
           >
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-XXXXX-Y', 'auto');
+          ga('send', 'pageview');
+        `}
+            </Script>
             <h1>Burger King Combo 1</h1>
-            <Card >
+            <Card>
               <CardMedia
                 component="img"
                 // height="100%"
